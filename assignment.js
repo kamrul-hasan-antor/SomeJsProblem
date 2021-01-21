992f8ed5e1755d6fdaafdd9be79435f310326496
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2021-01-22 03:06:09 +0600

    almost done

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
 // https://github.com/kamrul-hasan-antor/assignmentThree
 
-// The kilometerToMeter problem solving is given below.
+// The "kilometerToMeter" problem solving is given below.
 
 function kilometerToMeter(kilometer) {
   if (kilometer < 0) {
@@ -10,5 +10,40 @@ function kilometerToMeter(kilometer) {
   return meter;
 }
 
-var result = kilometerToMeter(-5698);
+var result = kilometerToMeter();
 console.log(result);
+
+// The "budgetCalculator" problem solving is given below.
+
+function budgetCalculator(watch, phone, laptop) {
+  if ((watch, phone, laptop < 0)) {
+    return "Error: Please insert a positive value.";
+  }
+  var cost = watch * 50 + phone * 100 + laptop * 500;
+  return cost;
+}
+var totalCost = budgetCalculator();
+console.log(totalCost);
+
+// The "hotelCost" problem solving is given below.
+
+function hotelCost(days) {
+  var cost = 0;
+  if (days <= 10) {
+    cost = days * 100;
+  } else if (days <= 20) {
+    var firstTenDays = 10 * 100;
+    var remainingDays = days - 10;
+    var secondTenDaysCost = remainingDays * 80;
+    cost = firstTenDays + secondTenDaysCost;
+  } else if (days > 20) {
+    var firstTenDays = 10 * 100;
+    var secondTenDaysCost = 10 * 80;
+    var remainingDays = days - 20;
+    var moreThanTwentyDaysCost = remainingDays * 50;
+    cost = firstTenDays + secondTenDaysCost + moreThanTwentyDaysCost;
+  }
+  return cost;
+}
+var totalCost = hotelCost();
+console.log(totalCost);
