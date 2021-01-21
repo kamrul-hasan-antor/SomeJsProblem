f03e903775156f1df8e5051a8f9c490e0b4807e1
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2021-01-22 01:40:54 +0600

    kilometerToMeter

--- assignment.js
+++ assignment.js
@@ -1,1 +1,14 @@
-console.log(23);
+// https://github.com/kamrul-hasan-antor/assignmentThree
+
+// The kilometerToMeter problem solving is given below.
+
+function kilometerToMeter(kilometer) {
+  if (kilometer < 0) {
+    return "Error: Please insert a positive value. Distance cannot be negetive.";
+  }
+  var meter = kilometer * 1000;
+  return meter;
+}
+
+var result = kilometerToMeter(-5698);
+console.log(result);
